7fdb867abf6042f5883a8097c5c1ec2344823541
Jacob Wahlman <jacwa448@parlomba3.ida.liu.se>
2018-02-14 14:36:53 +0100

    Added Dice class

---
+++ Dice.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+/**
+ * Controls the dies used in the game. Ability to use n-sized dies.
+ */
+
+public class Dice
+{
+    private Random randint;
+    private int dieSize;
+
+    public Dice(int dieSize) {
+        this.randint = new Random();
+        this.dieSize = dieSize;
+    }
+
+    private int throwDie() {
+        return randint.nextInt((this.dieSize + 1) -1) + 1; // A random number between 1 and including 6
+    }
+}
