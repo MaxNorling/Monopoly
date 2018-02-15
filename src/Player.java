e1128150f0957ffcd6014fa74a6d40f25f82a3c1
Jacob Wahlman <jacwa448@parlomba2.ida.liu.se>
2018-02-15 16:05:19 +0100

    Added further logic to bank and player

--- src/Player.java
+++ src/Player.java
@@ -8,23 +8,44 @@ public class Player
 {
     private int playerMoney;
     private int currentTile;
-    private ArrayList<Tile> ownedTiles;
+    private int playerWorth;
+    private ArrayList<HouseTile> ownedTiles;
 
     public Player() {
         this.playerMoney = playerMoney; // Should be assigned to a starting money function
-        this.currentTile = currentTile; // Should be assigned to the starting tile "GO"
+        this.currentTile = 0; // Should be assigned to the starting tile "GO"
     	this.ownedTiles = new ArrayList<>();
+    	this.playerWorth = playerWorth();
     }
 
     public int getPlayerMoney() {
         return this.playerMoney;
     }
 
+    public void setPlayerMoney(int amount) {
+        this.playerMoney += amount;
+    }
+
     public int getCurrentTile() {
         return this.currentTile;
     }
 
+    public void removeTile() {
+
+    }
+
     public void getOwnedTiles() {} // Should return a Tile object
+
+    public int playerWorth() {
+        int sumMoney = playerMoney;
+
+        for (HouseTile tile: ownedTiles) {
+            sumMoney += tile.getPrice(); // Change to resell value instead
+        }
+
+        return sumMoney;
+
+    } // Should sum all the stuff a player owns. to be used in risk-analysis for the bank.
 
     public void moveToTile(int amountOfTiles) {}
 
