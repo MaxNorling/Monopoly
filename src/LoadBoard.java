41f5708cb9c36138d291b619014b7032b9008954
Jacob Wahlman <jacwa448@parlomba2.ida.liu.se>
2018-02-21 11:08:12 +0100

    Fixed merge errors and added more functionality to loadboard

--- src/LoadBoard.java
+++ src/LoadBoard.java
@@ -10,10 +10,18 @@ import java.util.ArrayList;
  */
 public class LoadBoard
 {
-    private List<HouseTile> houseTiles;
 
-    public LoadBoard() {
-	this.houseTiles = new ArrayList<>();
+    private ArrayList<HouseTile> boardTiles;
+    private TileMaker tileMaker;
+    private Board board;
+    private int tileX;
+    private int tileY;
+
+    public LoadBoard(Board board) {
+	this.boardTiles = new ArrayList<>();
+	this.board = board;
+	this.tileX = 0;
+	this.tileY = 0;
     }
 
     public void readTileInformation() {
@@ -48,7 +56,23 @@ public class LoadBoard
 		    }
 		}
 
-		createTile(streetName, Integer.parseInt(price), Color.getColor(color));
+		switch (color) {
+		    case "Blue":
+			createTopTile(streetName, Integer.parseInt(price), Color.getColor(color));
+			break;
+		    case "Red":
+			createBottomTile(streetName, Integer.parseInt(price), Color.getColor(color));
+			break;
+		    case "Orange":
+			createLeftTile(streetName, Integer.parseInt(price), Color.getColor(color));
+			break;
+		    case "Pink":
+			createRightTile(streetName, Integer.parseInt(price), Color.getColor(color));
+			break;
+		    default:
+			throw new IllegalArgumentException("Bad color.");
+		}
+
 		tileInfo = new ArrayList<>();
 
 	    } else {
@@ -58,7 +82,53 @@ public class LoadBoard
 	scanner.close();
     }
 
-    public void createTile(String streetName, int price, Color color) {
-	//houseTiles.add(new HouseTile(0, 0, (650 / 13) / 2, (650 / 13) / 2, price, color, streetName));
+    public HouseTile getLastAddedTile() {
+	if (!boardTiles.isEmpty()) {
+	    return boardTiles.get(boardTiles.size() - 1);
+	}
+
+	return null;
+    }
+
+    public void createBottomTile(String streetName, int price, Color color) {
+	if (!getLastAddedTile().getColor().equals(color)) {
+	    tileX = 0;
+	}
+
+	boardTiles.add(tileMaker.makeBottomHouseTile(board.getTileSize(), tileX, 5, Color.RED, price, streetName));
+	tileX += 1;
+    }
+
+    public void createRightTile(String streetName, int price, Color color) {
+	if (getLastAddedTile() != null && !getLastAddedTile().getColor().equals(color)) {
+	    tileY = 0;
+	}
+
+	boardTiles.add(tileMaker.makeRightHouseTile(board.getTileSize(), 5, tileY, Color.PINK, price, streetName));
+	tileY += 1;
+    }
+
+    public void createLeftTile(String streetName, int price, Color color) {
+	if (!getLastAddedTile().getColor().equals(color)) {
+	    tileY = 0;
+	}
+
+	boardTiles.add(tileMaker.makeLeftHouseTile(board.getTileSize(), 0, tileY, Color.ORANGE, price, streetName));
+	tileY += 1;
+    }
+
+    public void createTopTile(String streetName, int price, Color color) {
+	if (getLastAddedTile() != null && !getLastAddedTile().getColor().equals(color)) {
+	    tileX = 0;
+	}
+
+	boardTiles.add(tileMaker.makeTopHouseTile(board.getTileSize(), tileX, 0, Color.BLUE, price, streetName));
+	tileX += 1;
+    }
+
+    public static void main(String... args) {
+        LoadBoard loadBoard = new LoadBoard(new Board(10));
+        loadBoard.readTileInformation();
+        System.out.println(loadBoard.boardTiles);
     }
 }
